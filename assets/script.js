ca8bc4a127461d1ac393060ef683f485bd4471e2
thlgeo <theolenglart587@gmail.com>
2023-03-21 21:47:51 +0100

    modif dark mode

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
+if(sessionStorage.getItem("darkMode") == null)
+{
+  alert(1);
+  sessionStorage.setItem("darkMode","light");
+}
+let theme = sessionStorage.getItem("darkMode");
+let r = document.querySelector(':root');
+
+if(theme == "dark")
+{
+  r.style.setProperty('--color-darkMode','white');
+  r.style.setProperty('--background-darkMode','black');
+  r.style.setProperty('--color-hover-darkMode','black');
+  r.style.setProperty('--background-hover-darkMode','white');
+  document.getElementById("darkMode").classList.add("fa-sun");
+  document.getElementById("darkMode").classList.remove("fa-moon");
+}
+else if(theme === "light")
+{
+  r.style.setProperty('--color-darkMode','black');
+  r.style.setProperty('--background-darkMode','white');
+  r.style.setProperty('--color-hover-darkMode','white');
+  r.style.setProperty('--background-hover-darkMode','black');
+  document.getElementById("darkMode").classList.remove("fa-sun");
+  document.getElementById("darkMode").classList.add("fa-moon");
+}
+
 document.addEventListener("mousemove", parallax);
 function parallax(event) {
   this.querySelectorAll(".parallax span").forEach((shift) => {
@@ -9,11 +36,32 @@ function parallax(event) {
   });
 }
 
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+      begin = dc.indexOf(prefix);
+      if (begin != 0) return null;
+  }
+  else
+  {
+      begin += 2;
+      var end = document.cookie.indexOf(";", begin);
+      if (end == -1) {
+      end = dc.length;
+      }
+  }
+  // because unescape has been deprecated, replaced with decodeURI
+  //return unescape(dc.substring(begin + prefix.length, end));
+  return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
 function darkMode()
 {
     let r = document.querySelector(':root');
     let rs = getComputedStyle(r);
-    if(rs.getPropertyValue('--color-darkMode') == "black")
+    if(sessionStorage.getItem("darkMode") === "light")
     {
       r.style.setProperty('--color-darkMode','white');
       r.style.setProperty('--background-darkMode','black');
@@ -21,8 +69,9 @@ function darkMode()
       r.style.setProperty('--background-hover-darkMode','white');
       document.getElementById("darkMode").classList.add("fa-sun");
       document.getElementById("darkMode").classList.remove("fa-moon");
+      sessionStorage.setItem("darkMode","dark");
     }
-    else
+    else if(sessionStorage.getItem("darkMode") === "dark")
     {
       r.style.setProperty('--color-darkMode','black');
       r.style.setProperty('--background-darkMode','white');
@@ -30,42 +79,6 @@ function darkMode()
       r.style.setProperty('--background-hover-darkMode','black');
       document.getElementById("darkMode").classList.remove("fa-sun");
       document.getElementById("darkMode").classList.add("fa-moon");
+      sessionStorage.setItem("darkMode","light");
     }
 }
-
-// function toRectangle(event)
-// {
-//   let elements = document.getElementsByClassName("rectangle");
-//   if(event.target.tagName == "LI")
-//   {
-//     if(elements.length > 0)
-//     {
-//       removeRectangle(elements);
-//     }
-//     else
-//     {
-//       event.target.classList.toggle("rectangle");
-//     }
-    
-//   }
-//   else if(event.target.tagName == "I" || event.target.tagName == "IMG")
-//   {
-//     if(elements.length > 0)
-//     {
-//       removeRectangle(elements);
-//     }
-//     else
-//     {
-//       event.target.parentElement.classList.toggle("rectangle");
-//     }
-//   }
-//   else
-//   {
-//     removeRectangle(elements);
-//   }
-// }
-
-// function removeRectangle(elements)
-// {
-//   for(let i=0;i<elements.length;i++) elements[i].classList.remove("rectangle");
-// }
