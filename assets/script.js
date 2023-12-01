246bcbf117780e786f2065fa85387c042d9e0a68
Theo Lenglart <theo.lenglart.etu@univ-lille.fr>
2023-12-01 09:56:38 +0100

    ajout caroussel infini

--- assets/script.js
+++ assets/script.js
@@ -162,3 +162,12 @@ function popout(event) {
     document.getElementById("fleche").style.display = "flex";
   }
 }
+
+const swiper = new Swiper('.sample-slider',{
+  loop: true,
+    autoplay: {
+        delay: 0,
+    },
+    speed: 3000,          //add
+    slidesPerView: 7,
+});
